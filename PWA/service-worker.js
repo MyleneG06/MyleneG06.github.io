54ef5243214944d7dfedf8ca1410c37330e1c15f
Mylène GERVAISE <mylene.gervaise@gmail.com>
2022-02-21 16:07:47 +0100

    service-worker.js

---
+++ PWA/service-worker.js
@@ -0,0 +1,15 @@
+
+self.addEventListener('install', function
+(event) { // Install
+    
+});
+
+self.addEventListener('activate', function
+(event) { // Activate
+    
+});
+
+self.addEventListener('fetch', function
+(event) { // Request from main thread
+    
+});
